ba1e7d913c2d83e96e799b20791271aef8063151
ghiocgeorge <ghiocgeorge@gmail.com>
2021-02-25 23:01:45 +0200

    Add Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,94 @@
 <template>
   <v-app id="app">
     <v-container id="container" fluid>
-      <v-row>
-        <v-col cols="auto" class="mr-auto">
-          <v-icon left id="backBtn" @click="action"> mdi-arrow-left </v-icon>
-          <name>Name</name>
-        </v-col>
+        <v-row>
+          <v-col cols="auto" class="mr-auto">
+            <v-icon left id="back" @click="action"> mdi-arrow-left </v-icon>
+            <name>Name</name>
+          </v-col>
 
-        <v-col cols="auto">
-          <button id="delBtn" v-ripple class="elevation-2 py-2 px-4" @click="action">
-            Delete List
-          </button>
-        </v-col>
-      </v-row>
-
+          <v-col cols="auto" class="mt-3">
+            <button id="delBtn" v-ripple class="elevation-2 py-2 px-4" @click="action">
+              Delete List
+            </button>
+          </v-col>
+        </v-row>
       <v-card id="card" class="elevation-8">
         <Menu/>
         <HelloWorld/>
       </v-card>
-    </v-container>
+
+      </v-container>
+    <v-main>
+      
+    </v-main>
   </v-app>
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld';
-  import Menu from './components/Menu';
+import HelloWorld from './components/HelloWorld';
+import Menu from './components/Menu';
 
-  export default {
-    name: 'App',
+export default {
+  name: 'App',
 
-    components: {
-      Menu, HelloWorld, 
+  components: {
+    Menu, HelloWorld, 
+  },
+
+  methods: {
+    action () {
+      alert('You clicked a button without action!')
     },
-
-    methods: {
-      action () {
-        alert('You clicked a button without action!')
-      },
-    },
-  };
+  },
+};
 </script>
 
 <style>
-  #app {
-    background: #f6f6f6;
-  }
+#app {
+  background: #f6f6f6;
+}
+#back{
+  padding-bottom:12px; 
+  color:#21ce99; 
+  padding-right: 5px
+}
 
-  #container {
-    max-width: auto;
-    padding-block: 50px;
-    padding-inline: 50px;
-  }
+name {
+  font-size: 32px;
+  color: black
+}
 
-  #backBtn{
-    padding-bottom:12px; 
-    color:#21ce99; 
-    padding-right: 5px
-  }
+#delBtn {
+  padding-top: 25px;
+  background-color:#dc3545;
+  border-radius: 4px;
+  font-size:10px;
+  color: white;  
+}
 
-  name {
-    font-size: 32px;
-    color: black
-  }
+#card {
+  margin-top: 5px;
+}
 
-  #delBtn {
-    background-color:#dc3545;
-    border-radius: 4px;
-    font-size:13px;
-    color: white;  
-  }
+#container {
+  max-width: auto;
+  padding-block: 50px;
+  padding-inline: 50px;
+}
 
-  #card {
-    margin-top: 20px;
+/*
+  .container {
+    border: 1px solid green;
   }
-
-  /* 
-  container {
-      border: 1px solid green;
-    }
-    .row {
-      border: 1px solid red;
-    }
-    .col {
-      border: 1px solid green;
-    }
-    .card {
-      border: 1px solid blue;
-    }
-  */
+  .card {
+    border: 1px solid yellow;
+  }
+  .row {
+    border: 1px solid red;
+  }
+  .col {
+    border: 1px solid blue;
+  } 
+*/
 </style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,12 +3,12 @@
     <v-row id="menu">
       <button @click="action">
         <dropBTN class="mb-3">Manage contacts</dropBTN>
-        <v-icon left large id="dropIcon"> mdi-menu-down </v-icon>
+        <v-icon left medium id="dropIcon"> mdi-menu-down </v-icon>
       </button>
 
       <button @click="action">
         <dropBTN class="mb-3"> Add contacts </dropBTN>
-        <v-icon left large id="dropIcon"> mdi-menu-down </v-icon>
+        <v-icon left medium id="dropIcon"> mdi-menu-down </v-icon>
       </button>
     </v-row>
   </v-container>
@@ -26,20 +26,21 @@
 
 <style>
   #menu {
-    padding: 10px;
+    padding: 12px;
+    padding-bottom: 5px;
     border-bottom: 3px solid #cccccc;
   }
 
   dropBTN {
     padding-left: 20px;
-    padding-top: 10px;
-    font-size: 22px;
+    padding-top: 50px;
+    font-size: 16px;
     font-family: 'Lato';
     color: black
   }
 
   #dropIcon{
-    padding-bottom:2px; 
+    padding-bottom: 5px; 
     color: black; 
     padding-right: 5px
   }
